08b776d4dd209e15c770bb4679437deb0d7c9bc0
Yaphet09 <japhet.katigbak@elev.ga.ntig.se>
2020-03-23 10:44:34 +0100

    Flyttat filler till js-mapp

---
+++ javascript/javascript-exempel.js
@@ -0,0 +1,15 @@
+function authorDialog(){
+    alert("Japhet har gjort denna sida år 2020.");
+  }
+
+  function changecolor(color){
+    document.getElementById("stycke").style.backgroundColor = color;
+    document.getElementById("rubrik").innerHTML = "Japhet" ; 
+   } 
+
+   function pictureChange(){
+       
+     document.getElementById("bild").src ="saitama.ok2.png";
+   } 
+  
+   // japhet, 16 03 2020
