c875d74fd8eb10d9b131c5e9f6b838ec0fa1c816
Yaphet09 <japhet.katigbak@elev.ga.ntig.se>
2020-03-25 13:20:49 +0100

    uppgift 5 - 7

--- javascript/javascript-exempel.js
+++ javascript/javascript-exempel.js
@@ -1,14 +1,14 @@
 function authorDialog(){
     alert("Japhet har gjort denna sida år 2020.");
-  }
+}
 
-  function changecolor(color){
+function changecolor(color){
     document.getElementById("stycke").style.backgroundColor = color;
     document.getElementById("rubrik").innerHTML = "Japhet" ; 
-   } 
-   var value = true;
-   function pictureChange() {                                                     
+} 
 
+   var value = true;
+function pictureChange() {                                                     
     var image = document.getElementById('pictureChange');
 
     if (image.src.match("Saitama.ok1.png")) {
@@ -18,7 +18,23 @@ function authorDialog(){
         image.src = "Saitama.ok1.png";
     }
 }
-     
+
+function disname() {
+    var x, text;
+
+              x = document.getElementById("name").value;
+
+              text = "Hej " + x + "!";
+
+              alert(text);
+              
+              document.getElementById("demo").innerHTML = text;           
+
+
+}
+    
+
+
   
   
    // japhet, 16 03 2020
