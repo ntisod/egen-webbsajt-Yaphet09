086319dfe05dc89157606c4ae8e83da287d6a501
Yaphet09 <japhet.katigbak@elev.ga.ntig.se>
2020-05-24 09:54:15 +0200

    validator test

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-function hasTouch() {
-    return 'ontouchstart' in document.documentElement
-           || navigator.maxTouchPoints > 0
-           || navigator.msMaxTouchPoints > 0;
-}
 
-if (hasTouch()) { // remove all the :hover stylesheets
-    try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
-
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
-
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
-}
-
+/* Funktionen gör att information i contact-form försvinner*/
 function cleartext(){
     document.getElementById("text-name").value = "";
     document.getElementById("text-email").value = "";
